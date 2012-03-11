fd45ba664956de210a69b81121a219e2937b4ae7
Eric Gallager <egallager@comcast.net>
2012-03-11 07:36:22 -0400

    add official Dropbox iOS SDK framework and some other stuff
    more code!

---
+++ Xcode/Dropbox.c
@@ -0,0 +1,32 @@
+/*
+ *  Dropbox.c
+ *  Dropbox
+ *
+ *  Created by Eric Gallager on 3/11/12.
+ *  Copyright 2012 George Washington University. All rights reserved.
+ *
+ */
+
+// #include all the headers extracted by ClassDump, just in case:
+#include "Dropbox.h"
+#include "CDStructures.h"
+#include "DropboxContextMenuState.h"
+#include "DropboxIKImageBrowserCell.h"
+#include "DropboxLargeToolbarButton.h"
+#include "DropboxMenu.h"
+#include "DropboxMenuContext.h"
+#include "DropboxMenuItem.h"
+#include "DropboxNSMenu.h"
+#include "DropboxSmallToolbarImageView.h"
+#include "DropboxTListViewIconAndTextCell.h"
+#include "DropboxToolbar.h"
+#include "DropboxToolbarController.h"
+#include "DropboxToolbarItem.h"
+#include "DummyClass.h"
+#include "NSMenuDelegate-Protocol.h"
+#include "NSObject-Protocol.h"
+
+int main(void)
+{
+	printf(const char *) // Put stuff here
+}
---
+++ Xcode/Dropbox.h
@@ -0,0 +1,11 @@
+/*
+ *  Dropbox.h
+ *  Dropbox
+ *
+ *  Created by Eric Gallager on 3/11/12.
+ *  Copyright 2012 George Washington University. All rights reserved.
+ *
+ */
+
+#include <stdio.h>
+
