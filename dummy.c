6bdb3c776df9f889f63009f82e69f9a2e778263f
Eric Gallager <egallager@comcast.net>
2012-05-08 22:19:20 -0400

    hm, I thought I had already synced these...

---
+++ dummy.c
@@ -0,0 +1,49 @@
+/*
+ *  dummy.c
+ *  Problem 4
+ *
+ *  Created by Eric Gallager on 5/7/12.
+ *  Copyright 2012 George Washington University. All rights reserved.
+ *
+ */
+
+#include "dummy.h"
+#define C 12
+
+/* For some reason Github thinks this project is Assembly just because I have a single assembly file in it...
+ * Anyway, I'm just making some dummy C files to see if I can get it to recognize that it's C again.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Making this comment extra long so the file will be bigger
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * There, that should be good
+ *
+ */
+
+//newline \n
+
