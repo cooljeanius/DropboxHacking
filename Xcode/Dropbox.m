3f4b3a8f77d3e26279bb64a837ded8e72338fffd
Eric Gallager <egallager@comcast.net>
2013-08-27 10:33:51 -0400

    Update Dropbox.m
    remove copyright from header template

--- Xcode/Dropbox.m
+++ Xcode/Dropbox.m
@@ -2,8 +2,7 @@
 //  Dropbox.m
 //  Dropbox
 //
-//  Created by Eric Gallager on 3/11/12.
-//  Copyright 2012 George Washington University. All rights reserved.
+//  Created by cooljeanius on 3/11/12.
 //
 
 #include "dropbox.h"
