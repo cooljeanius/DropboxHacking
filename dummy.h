c7c976c11ef32e15ea367e20285066be1e0c2be8
Eric Gallager <egallager@comcast.net>
2014-01-27 08:55:45 -0500

    Update dummy.h
    remove copyright from header template

--- dummy.h
+++ dummy.h
@@ -2,8 +2,7 @@
  *  dummy.h
  *  Problem 4
  *
- *  Created by Eric Gallager on 5/7/12.
- *  Copyright 2012 George Washington University. All rights reserved.
+ *  Created by cooljeanius on 5/7/12.
  *
  */
 
